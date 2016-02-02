7d3e7bd13d20a3a706d6eab37dcf4595daceca71
Ben Batschelet <ben@codeup.com>
2016-02-02 11:59:18 -0600

    Consolidate some of the spec code and examples

--- spec/python-prod-spec.rb
+++ spec/python-prod-spec.rb
@@ -3,19 +3,12 @@ require_relative "bootstrap"
 
 RSpec.configure do |config|
   config.before :suite do
-    AnsibleHelper.instance.playbook("playbooks/python-prod-playbook.yml", {
-      domain: "prod-test.dev",
-      copy_wsgi: true
-    })
+    AnsibleHelper.instance.playbook "playbooks/python-prod-playbook.yml", copy_wsgi: true
   end
 end
 
-describe command("nginx -t") do
-  # stderr?? Wtf nginx.
-  its(:stderr) { should match /configuration file \/etc\/nginx\/nginx\.conf syntax is ok/ }
-  its(:stderr) { should match /configuration file \/etc\/nginx\/nginx\.conf test is successful/ }
-
-  its(:exit_status) { should eq 0 }
+describe "Nginx config should be valid" do
+  include_examples "nginx::config"
 end
 
 describe command('printf "GET / HTTP/1.1\nHost: prod-test.dev\n\n" | nc 127.0.0.1 80') do
--- spec/python2-dev-spec.rb
+++ spec/python2-dev-spec.rb
@@ -3,20 +3,12 @@ require_relative "bootstrap"
 
 RSpec.configure do |config|
   config.before :suite do
-    AnsibleHelper.instance.playbook("playbooks/python-dev-playbook.yml", {
-      domain: "dev-test.dev",
-      copy_wsgi: true,
-      python_version: "2"
-    })
+    AnsibleHelper.instance.playbook "playbooks/python-dev-playbook.yml", { copy_wsgi: true, python_version: "2" }
   end
 end
 
-describe command("nginx -t") do
-  # stderr?? Wtf nginx.
-  its(:stderr) { should match /configuration file \/etc\/nginx\/nginx\.conf syntax is ok/ }
-  its(:stderr) { should match /configuration file \/etc\/nginx\/nginx\.conf test is successful/ }
-
-  its(:exit_status) { should eq 0 }
+describe "Nginx config should be valid" do
+  include_examples "nginx::config"
 end
 
 describe command('printf "GET / HTTP/1.1\nHost: dev-test.dev\n\n" | nc 127.0.0.1 80') do

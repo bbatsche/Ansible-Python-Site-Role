7b4d64b98c9cc533ea28271f037a85b896ff5fbd
Ben Batschelet <ben@codeup.com>
2016-03-23 09:50:13 -0500

    Update specs to use curl

--- spec/python2-dev-spec.rb
+++ spec/python2-dev-spec.rb
@@ -11,9 +11,8 @@ describe "Nginx config should be valid" do
   include_examples "nginx::config"
 end
 
-describe command('printf "GET / HTTP/1.1\nHost: dev-test.dev\n\n" | nc 127.0.0.1 80') do
-  # check headers
-  its(:stdout) { should match /^+HTTP\/1\.1 200 OK$/ }
+describe command('curl -i dev-test.dev') do
+  its(:stdout) { should match /^HTTP\/1\.1 200 OK$/ }
 
   its(:stdout) { should match /Phusion Passenger is serving Python 2 code on dev-test\.dev/ }
 end
